cb34c36a8fb15172543b67cf5fc5bd806884c522
Truong Minh <minh.truong@codecomplete.jp>
2023-05-08 14:46:01 +0700

    update schedule

--- app/src/main/java/com/example/momkid/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/com/example/momkid/ui/schedule/ScheduleFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,11 +14,30 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.momkid.R;
 import com.example.momkid.databinding.FragmentScheduleBinding;
 
+import java.util.ArrayList;
+
 public class ScheduleFragment extends Fragment {
 
+    private ListView lvSchedule;
+    private ArrayList<ScheduleViewModel> arrSchedule;
+    private ScheduleAdapter scheduleAdapter;
 
+    private View view;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_schedule,container,false);
+        view= inflater.inflate(R.layout.fragment_schedule,container,false);
+
+        lvSchedule=(ListView) view.findViewById(R.id.lv_schedule);
+        arrSchedule= new ArrayList<>();
+        arrSchedule.add(new ScheduleViewModel("7:00","Cho bé ăn sáng"));
+        arrSchedule.add(new ScheduleViewModel("8:00","Cho bé ngủ"));
+
+        scheduleAdapter=new ScheduleAdapter(getContext(),R.layout.item_schedule,arrSchedule);
+        lvSchedule.setAdapter(scheduleAdapter);
+
+
+
+
+        return view;
     }
 }
