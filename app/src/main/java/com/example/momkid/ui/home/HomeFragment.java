b2124408430daec1478d57cfe980399925a37a95
Thuthao <ngothithuthao202128@gmail.com>
2023-05-18 23:08:06 +0700

    done slideimg

--- app/src/main/java/com/example/momkid/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/momkid/ui/home/HomeFragment.java
@@ -21,10 +21,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.momkid.R;
 import com.example.momkid.databinding.FragmentHomeBinding;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+    ImageSlider imageSlider;
+
     private View view;
 
     private TextView txt_birthdate,txt_height,txt_weight;
@@ -33,7 +39,18 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home,container,false);
 
+        imageSlider = view.findViewById(R.id.image_slider);
 
+        ArrayList<SlideModel> imageList=new ArrayList<>();
+//        imageList.add(new SlideModel("https://fastly.picsum.photos/id/20/200/300.jpg?hmac=DFzxrn8j8N0vtdAloI4hYTGWP-nNrDqMbVcWjF2BLwA", "description 1", null));imageList.add(new SlideModel("https://fastly.picsum.photos/id/20/200/300.jpg?hmac=DFzxrn8j8N0vtdAloI4hYTGWP-nNrDqMbVcWjF2BLwA", "description 1", null));
+//        imageList.add(new SlideModel("https://fastly.picsum.photos/id/89/200/300.jpg?hmac=Mkt49bCquTyq1IBgwbLmaT43TeyQgXNig052fowQB1M", "description 1", null));imageList.add(new SlideModel("https://fastly.picsum.photos/id/20/200/300.jpg?hmac=DFzxrn8j8N0vtdAloI4hYTGWP-nNrDqMbVcWjF2BLwA", "description 2", null));
+        imageList.add(new SlideModel(R.drawable.imgone,null));
+        imageList.add(new SlideModel(R.drawable.imgtwo,null));
+        imageList.add(new SlideModel(R.drawable.imgthree,null));
+        imageList.add(new SlideModel(R.drawable.imgfour,null));
+        imageList.add(new SlideModel(R.drawable.imgfive,null));
+
+        imageSlider.setImageList(imageList);
         return view;
 
     }
